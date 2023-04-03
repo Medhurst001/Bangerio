840911f0695685c68bfa863d4a67ee06a28e6b94
Medhurst001 <129784589+Medhurst001@users.noreply.github.com>
2023-04-04 05:03:54 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,41 +3,13 @@ import 'prismjs/themes/prism-tomorrow.css';
 
 import Script from 'next/script'
 
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <>
-//       <span className="theme-bejamas" />
-//       <Component {...pageProps} />
-//     </>
-//   );
-// }
-
-// export default MyApp;
-
 function MyApp({ Component, pageProps }) {
-    return (
-  <>
-    <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
-      <Script
-        id='google-analytics'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-FQ3GHWYDES', {
-          page_path: window.location.pathname,
-        });
-        `,
-        }}
-      />
-    <Layout>
-    <span className="theme-bejamas" />
-    <Component {...pageProps} />
-    </Layout>
-  </>
-  )
-  }
-  
-  export default MyApp
+  return (
+    <>
+      <span className="theme-bejamas" />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
