79be5609342a96311f7c44e6c1ba1df1fa751f2d
Medhurst001 <129784589+Medhurst001@users.noreply.github.com>
2023-04-04 07:21:32 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
 
-import Script from 'next/script'
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -13,34 +11,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
-
-// import '../styles/globals.css'
-// import 'prismjs/themes/prism-tomorrow.css';
-// import Script from 'next/script'
- 
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <>
-// //     <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
-// //     <Script
-// //       id='google-analytics'
-// //       strategy="afterInteractive"
-// //       dangerouslySetInnerHTML={{
-// //         __html: `
-// //           window.dataLayer = window.dataLayer || [];
-// //           function gtag(){dataLayer.push(arguments);}
-// //           gtag('js', new Date());
-// //           gtag('config', 'G-FQ3GHWYDES', {
-// //             page_path: window.location.pathname,
-// //           });
-// //         `,
-// //         }}
-// //     />
-//     <span className="theme-bejamas" />
-//     <Component {...pageProps} />
-    
-//     </>
-//   )
-// }
- 
-// export default MyApp
