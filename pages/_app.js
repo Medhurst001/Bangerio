a41674c14442a841dc45c49d42331313e0971f02
Medhurst001 <129784589+Medhurst001@users.noreply.github.com>
2023-04-04 06:57:50 +1000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,46 +1,46 @@
-// import '../styles/globals.css';
-// import 'prismjs/themes/prism-tomorrow.css';
-
-// import Script from 'next/script'
-
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <>
-//       <span className="theme-bejamas" />
-//       <Component {...pageProps} />
-//     </>
-//   );
-// }
-
-// export default MyApp;
-
-import '../styles/globals.css'
+import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
+
 import Script from 'next/script'
- 
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-//     <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
-//     <Script
-//       id='google-analytics'
-//       strategy="afterInteractive"
-//       dangerouslySetInnerHTML={{
-//         __html: `
-//           window.dataLayer = window.dataLayer || [];
-//           function gtag(){dataLayer.push(arguments);}
-//           gtag('js', new Date());
-//           gtag('config', 'G-FQ3GHWYDES', {
-//             page_path: window.location.pathname,
-//           });
-//         `,
-//         }}
-//     />
-    <span className="theme-bejamas" />
-    <Component {...pageProps} />
-    
-    </>
-  )
+  return (
+    <>
+      <span className="theme-bejamas" />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
+
+// import '../styles/globals.css'
+// import 'prismjs/themes/prism-tomorrow.css';
+// import Script from 'next/script'
  
-export default MyApp
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <>
+// //     <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+// //     <Script
+// //       id='google-analytics'
+// //       strategy="afterInteractive"
+// //       dangerouslySetInnerHTML={{
+// //         __html: `
+// //           window.dataLayer = window.dataLayer || [];
+// //           function gtag(){dataLayer.push(arguments);}
+// //           gtag('js', new Date());
+// //           gtag('config', 'G-FQ3GHWYDES', {
+// //             page_path: window.location.pathname,
+// //           });
+// //         `,
+// //         }}
+// //     />
+//     <span className="theme-bejamas" />
+//     <Component {...pageProps} />
+    
+//     </>
+//   )
+// }
+ 
+// export default MyApp
