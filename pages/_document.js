1205a4ef44d0a269cdb2548902fc22ab7489da3c
Medhurst001 <129784589+Medhurst001@users.noreply.github.com>
2023-04-04 06:38:56 +1000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -6,15 +6,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="theme-compiled">
         <Head />
-      <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FQ3GHWYDES"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-FQ3GHWYDES');
-        </script>
+     
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
